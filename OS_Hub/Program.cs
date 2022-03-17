b00d47d3332424670dded85c41e0cedcc6a8ce8e
Дмитрий Балабанов <dmittrj@outlook.com>
2022-03-17 19:09:01 +0300

    Menu

--- OS_Hub/Program.cs
+++ OS_Hub/Program.cs
@@ -7,6 +7,9 @@ namespace OS_Hub
         private static int b;
         private static int c;
         private static int i_max;
+        private static int cur_position = 1;
+        private static string[] OS_Results = { "undef", "undef" , "undef" , "undef" };
+        private static string[] OS_Time = { "undef", "undef", "undef", "undef" };
         private static void OS_Initial()
         {
             Console.WriteLine("\n Эта программа посчитает вам значение этого выражения:");
@@ -33,15 +36,55 @@ namespace OS_Hub
             }
             catch { goto _OS_imax; }
         }
+        
+        private static void OS_PrintCursor(int p)
+        {
+            if (p == cur_position) Console.Write(">");
+            else Console.Write(" ");
+        }
+        private static void OS_PrintTable()
+        {
+            while (true) {
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine("  Язык        Результат   Время работы");
+                OS_PrintCursor(1);
+                Console.Write(" C++         ");
+                Console.Write(OS_Results[0]);
+                Console.Write("       ");
+                Console.WriteLine("undef");
+                OS_PrintCursor(2);
+                Console.Write(" Java        ");
+                Console.Write(OS_Results[1]);
+                Console.Write("       ");
+                Console.WriteLine("undef");
+                OS_PrintCursor(3);
+                Console.Write(" Python      ");
+                Console.Write(OS_Results[2]);
+                Console.Write("       ");
+                Console.WriteLine("undef");
+                OS_PrintCursor(4);
+                Console.Write(" Assembler   ");
+                Console.Write(OS_Results[3]);
+                Console.Write("       ");
+                Console.WriteLine("undef");
+                ConsoleKey key = Console.ReadKey().Key;
+                switch (key)
+                {
+                    case ConsoleKey.DownArrow:
+                        cur_position++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        cur_position--;
+                        break;
+                }
+            }
+        }
+        
         static void Main(string[] args)
         {
             OS_Initial();
-            Console.WriteLine();
-            Console.WriteLine(" Язык        Результат   Время работы");
-            Console.WriteLine(" C++         undef       undef");
-            Console.WriteLine(" Java        undef       undef");
-            Console.WriteLine(" Python      undef       undef");
-            Console.WriteLine(" Assembler   undef       undef");
+            OS_PrintTable();
         }
     }
 }
