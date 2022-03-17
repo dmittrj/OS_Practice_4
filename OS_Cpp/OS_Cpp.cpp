d8b15b5b0053c7ac3f7d78494c3946ea4156e8a9
Дмитрий Балабанов <dmittrj@outlook.com>
2022-03-17 18:29:47 +0300

    Добавьте файлы проекта.

---
+++ OS_Cpp/OS_Cpp.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+const int NUMBEROFOPERATIONS = 100000000;
+
+int f(int arg, int b, int c) {
+    if (arg == 0) return 0;
+    int a = 0;
+    for (int i = 1; i < NUMBEROFOPERATIONS; i++) {
+        a += 2 * b + c - i;
+    }
+    return f(arg - 1, b, c) + a;
+}
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
---
+++ OS_Hub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OS_Hub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine(" Язык        Результат   Время работы");
+            Console.WriteLine(" C++         undef       undef");
+            Console.WriteLine(" Java        undef       undef");
+            Console.WriteLine(" Python      undef       undef");
+            Console.WriteLine(" Assembler   undef       undef");
+        }
+    }
+}
