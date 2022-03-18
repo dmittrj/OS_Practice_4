9b73241a69f3a7d430e6d1e1385233662000e4d0
Дмитрий Балабанов <dmittrj@outlook.com>
2022-03-18 22:55:12 +0300

    Now the hub gets information about C plus plus results

--- OS_Hub/Program.cs
+++ OS_Hub/Program.cs
@@ -14,6 +14,17 @@ namespace OS_Hub
         private static int cur_position = 1;
         private static string[] OS_Results = { "undef", "undef" , "undef" , "undef" };
         private static string[] OS_Time = { "undef", "undef", "undef", "undef" };
+
+        private static void OS_ParseFile(FileInfo file, OS_Language lang)
+        {
+            using FileStream fstream = File.OpenRead(file.FullName);
+            byte[] array = new byte[fstream.Length];
+            fstream.Read(array, 0, array.Length);
+            string textFromFile = System.Text.Encoding.Default.GetString(array);
+            OS_Results[(int)lang - 1] = textFromFile.Substring(0, textFromFile.IndexOf(","));
+            OS_Time[(int)lang - 1] = textFromFile[(textFromFile.IndexOf(",") + 1)..];
+        }
+        
         private static void OS_Initial()
         {
             Console.WriteLine("\n Эта программа посчитает вам значение этого выражения:");
@@ -91,28 +102,34 @@ namespace OS_Hub
         private static void OS_PrintTable()
         {
             while (true) {
+                FileInfo checks = new(path + @"\OS_Hub\bin\Debug\net5.0\OS_CppResult.txt");
+                if (checks.Exists)
+                {
+                    OS_ParseFile(checks, OS_Language.CPlusPlus);
+                }
                 Console.Clear();
                 Console.WriteLine();
-                Console.WriteLine("  Язык        Результат   Время работы");
+                Console.WriteLine("  Язык        Результат     Время работы");
                 OS_PrintCursor(1);
                 Console.Write(" C++         ");
                 Console.Write(OS_Results[0]);
-                Console.Write("       ");
-                Console.WriteLine("undef");
+                for (int i = 0; i < 14 - OS_Results[0].Length; i++)
+                    Console.Write(" ");
+                Console.WriteLine(OS_Time[0] + " сек.");
                 OS_PrintCursor(2);
                 Console.Write(" Java        ");
                 Console.Write(OS_Results[1]);
-                Console.Write("       ");
+                Console.Write("         ");
                 Console.WriteLine("undef");
                 OS_PrintCursor(3);
                 Console.Write(" Python      ");
                 Console.Write(OS_Results[2]);
-                Console.Write("       ");
+                Console.Write("         ");
                 Console.WriteLine("undef");
                 OS_PrintCursor(4);
                 Console.Write(" Assembler   ");
                 Console.Write(OS_Results[3]);
-                Console.Write("       ");
+                Console.Write("         ");
                 Console.WriteLine("undef");
                 ConsoleKey key = Console.ReadKey().Key;
                 switch (key)
